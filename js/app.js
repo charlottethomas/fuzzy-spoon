c88cb477ada1c78546a0d534d2583b5b7ed8fb39
Clement Lepissier <clem.lepissier@gmail.com>
2017-11-23 00:52:52 +0100

    generate new version

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var jQuery = null;
+window.onload=function(){
+  var elements = document.getElementsByClassName("is-zoomable")
+  var nZoomables = elements.length
+  var zoomables = []
+
+  for (var i = 0; i < nZoomables; i++) {
+    zoomables.push({
+      id: elements[i].id,
+      active: false
+    })
+  }
+
+  zoomables.forEach(function(zoomable) {
+    zoomable.imgZoom = new ImageZoom('#'+zoomable.id, {
+      defaultDisabled: true,
+      toggleOnClick: true
+    });
+  })
+}
